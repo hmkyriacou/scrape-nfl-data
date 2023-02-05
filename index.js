3036fabc7d623429d8f0e7256f439ed7755f2283
harrison <hmkyriacou14@gmail.com>
2023-02-05 16:43:51 -0500

    Done

---
+++ index.js
@@ -0,0 +1,89 @@
+const cheerio = require('cheerio')
+const fs = require('fs/promises')
+
+years = [2022] //, 2021, 2020, 2019, 2018, 2017, 2016]
+
+// ** data structure
+// *  JSON -
+// {
+//     year: {
+//         week_num : {
+//             away_home : {
+//                 winner: "home" or "away",
+//                 home_score:
+//                 away_score:
+//                 date:
+//             }
+//         }
+//     }
+// }
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+async function main() {
+    let data = {}
+
+    for (const year of years) {
+        let url = `https://www.pro-football-reference.com/years/${year}`
+        //console.log(url)
+        let html = await fetch(url).then(d => d.text())
+        //let html = await fs.readFile("./week_1.htm", { encoding: 'utf8' })
+
+
+        let $ = cheerio.load(html)
+        //console.log($.html())
+        //console.log($(".game_summaries").children().length)
+        let weeks_this_yr = $(".condensed > div:nth-child(2) > ul:nth-child(8)").children().length
+
+        let year_data = {}
+
+        for (let week_num = 1; week_num <= weeks_this_yr; week_num++) {
+
+            console.log(`Starting week ${week_num}, ${year}`)
+            await sleep(1000)
+
+            url = `https://www.pro-football-reference.com/years/${year}/week_${week_num}.htm`
+            html = await fetch(url).then(d => d.text())
+
+            $ = cheerio.load(html)
+
+            let week_data = {}
+
+            //console.log($(".game_summaries").children().length)
+
+            for (let game_num = 1; game_num <= $(".game_summaries").children().length; game_num++) {
+
+                let game_data = {}
+
+                // Date
+                let date = $(`div.game_summary:nth-child(${game_num}) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)`).text()
+                // Away
+                let away = $(`div.game_summary:nth-child(${game_num}) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)`).text()
+                // Away Score
+                let away_score = $(`div.game_summary:nth-child(${game_num}) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)`).text()
+                // Home
+                let home = $(`div.game_summary:nth-child(${game_num}) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(1)`).text()
+                // Home Score
+                let home_score = $(`div.game_summary:nth-child(${game_num}) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)`).text()
+
+                game_data = {
+                    date,
+                    away_score,
+                    home_score
+                }
+                week_data[`${away}_${home}`] = game_data
+            }
+            year_data[week_num] = week_data
+
+        }
+        data[year] = year_data
+    }
+
+    console.log(data)
+}
+
+main()
