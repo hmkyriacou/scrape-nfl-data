f0675eb4a94babb32d98b82bb6aeac273960c181
harrison <hmkyriacou14@gmail.com>
2023-02-06 22:27:29 -0500

    can pull and update data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cheerio = require('cheerio')
 const fs = require('fs/promises')
 
-years = [2022] //, 2021, 2020, 2019, 2018, 2017, 2016]
+const years = [2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010]
 
 // ** data structure
 // *  JSON -
@@ -25,9 +25,53 @@ function sleep(ms) {
 }
 
 async function main() {
-    let data = {}
 
-    for (const year of years) {
+    // Read in data and determine what year/week to start at
+    let curData = await fs.readFile("./data.json", { encoding: 'utf8' })
+
+    let curDataObj = JSON.parse(curData)
+
+    let yearToStart = 'DONE'
+    let weekToStart = 'DONE'
+
+    //console.log(Object.keys(curDataObj[2021])[Object.keys(curDataObj[2021]).length - 1])
+
+    for (const year in curDataObj) {
+        let lastWeekDefined = Object.keys(curDataObj[year])[Object.keys(curDataObj[year]).length - 1]
+        if (lastWeekDefined === undefined) {
+            yearToStart = year
+            continue
+        } else if (Object.keys(curDataObj[year][lastWeekDefined]).length === 0) {
+            yearToStart = year
+            for (let i = Object.keys(curDataObj[year]).length - 1; i > 0; i--) {
+                if (Object.keys(curDataObj[year][i]).length === 0) {
+                    //console.log(year, i, Object.keys(curDataObj[year][i]).length)
+                    continue
+                } else {
+                    weekToStart = i
+                    break
+                }
+            }
+            break
+        }
+    }
+
+    if (yearToStart === 'DONE') {
+        console.log("All done")
+        return
+    }
+
+    let yearsToDo = years.filter((v) => {
+        if (v > yearToStart) {
+            return false
+        } else {
+            return true
+        }
+    })
+
+    let data = curDataObj //if starting new, {}
+
+    for (const year of yearsToDo) {
         let url = `https://www.pro-football-reference.com/years/${year}`
         //console.log(url)
         let html = await fetch(url).then(d => d.text())
@@ -41,13 +85,23 @@ async function main() {
 
         let year_data = {}
 
-        for (let week_num = 1; week_num <= weeks_this_yr; week_num++) {
+        let startWeek = 1
+
+        if (year == yearToStart) {
+            if (weekToStart !== 'DONE') {
+                startWeek = weekToStart
+            }
+        }
+
+        for (let week_num = startWeek; week_num <= weeks_this_yr; week_num++) {
 
             console.log(`Starting week ${week_num}, ${year}`)
-            await sleep(1000)
+            //await sleep(1000)
 
             url = `https://www.pro-football-reference.com/years/${year}/week_${week_num}.htm`
             html = await fetch(url).then(d => d.text())
+            //html = await fs.readFile("./week_1.htm", { encoding: 'utf8' })
+
 
             $ = cheerio.load(html)
 
@@ -83,7 +137,9 @@ async function main() {
         data[year] = year_data
     }
 
-    console.log(data)
+    fs.writeFile('data_updated.json', JSON.stringify(data), 'utf8')
+
+    //console.log(data)
 }
 
 main()
