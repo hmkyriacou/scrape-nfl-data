ef04b4de49bc4a8a66cf9cbb77f4004bfe7c150c
harrison <hmkyriacou14@gmail.com>
2023-04-10 16:30:57 -0400

    better stadium coords

--- gen_meta_data.mjs
+++ gen_meta_data.mjs
@@ -1,22 +1,34 @@
-import fs from 'fs/promises'
-import fetch from 'node-fetch'
-import { parse } from "csv-parse/sync"
+import fs from "fs/promises";
+import fetch from "node-fetch";
+import { parse } from "csv-parse/sync";
 
 async function main() {
-    
-    let cur_meta_data = await fs.readFile("./meta-data.json", {encoding: 'utf8'})
+  let cur_meta_data = await fs.readFile("./meta-data.json", {
+    encoding: "utf8",
+  });
 
-    cur_meta_data = JSON.parse(cur_meta_data)
+  cur_meta_data = JSON.parse(cur_meta_data);
 
-    console.log(cur_meta_data)
+  let rawText = await fs.readFile("./nfl_stad_coords.csv", {
+    encoding: "utf8",
+  });
 
-    for (let key in cur_meta_data) {
+  const records = parse(rawText, {
+    columns: true,
+    skip_empty_lines: true,
+    trim: true,
+    record_delimiter: "\n",
+  });
 
-        
-        
-    }
+  for (const r of records) {
+    const k = Object.keys(cur_meta_data).find((e) => e.includes(r.Team));
+    cur_meta_data[k].location.lat = r.latitude;
+    cur_meta_data[k].location.lng = r.longitude;
+  }
 
-    //fs.writeFile('meta-data.json', JSON.stringify(newObj), 'utf8')
+  console.log(cur_meta_data);
+
+  fs.writeFile("meta-data.json", JSON.stringify(cur_meta_data), "utf8");
 }
 
-main()
+main();
